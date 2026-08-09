b5e9f79525828c0aa7e8b25ecd00798bd10a0663
zenovaeducations <zenovaeducations@gmail.com>
2026-08-09 19:12:21 +0530

    Update view.js

--- view/view.js
+++ view/view.js
@@ -63,40 +63,6 @@ togglePassword.addEventListener("click", () => {
 });
 
 
-// ==========================================================
-// SHA-256
-// ==========================================================
-
-async function hashPassword(password) {
-
-    const encoder =
-        new TextEncoder();
-
-    const data =
-        encoder.encode(password);
-
-    const hashBuffer =
-        await crypto.subtle.digest(
-            "SHA-256",
-            data
-        );
-
-    const hashArray =
-        Array.from(
-            new Uint8Array(hashBuffer)
-        );
-
-    return hashArray
-        .map(
-            byte =>
-                byte
-                    .toString(16)
-                    .padStart(2, "0")
-        )
-        .join("");
-
-}
-
 
 // ==========================================================
 // VERIFY PASSWORD
